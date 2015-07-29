7701fe620459b6e88c5bd33ca5e87e2a6dbeb4de
Kyle Swanson <k@ylo.ph>
2015-07-29 16:02:45 -0500

    0.0 after ADSR

--- adsr.c
+++ adsr.c
@@ -2,7 +2,9 @@
 
 float adsr_envelope(int A_LENGTH, int D_LENGTH, int S_LENGTH, int R_LENGTH, float S_LEVEL, int sample_number)
 {
-  if (sample_number > A_LENGTH + D_LENGTH + S_LENGTH) {
+  if (sample_number > A_LENGTH + D_LENGTH + S_LENGTH + R_LENGTH) {
+    return 0.0;  
+  } else if (sample_number > A_LENGTH + D_LENGTH + S_LENGTH) {
     /* RELEASE */
     return S_LEVEL * (1.0 - (log(1 + (((float) (sample_number - A_LENGTH - D_LENGTH - S_LENGTH) / R_LENGTH) * (M_E - 1)))));
   } else if (sample_number > A_LENGTH + D_LENGTH) {
